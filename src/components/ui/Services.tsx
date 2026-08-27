63fe9b42abdec811f8576260bb48b8c270a838f9
BHM Energy <bhmenergy@example.com>
2026-08-27 21:30:46 +0200

    Change Services title case to Usluge

--- src/components/ui/Services.tsx
+++ src/components/ui/Services.tsx
@@ -170,9 +170,9 @@ const Services = () => {
             initial={{ opacity: 0, y: -20 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, amount: 0.3 }}
-            className="text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-400 uppercase"
+            className="text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-400"
           >
-            USLUGE
+            Usluge
           </motion.h2>
           <motion.p 
             initial={{ opacity: 0, y: -20 }}
